4c12cacf656a5a68b2a1afa73229bc6560f5471c
ivveta <sweta-89-@mail.ru>
2022-05-15 15:04:45 +0300

    add fundumentals

---
+++ 1-fundumentals-of-testing/assertion-library.js
@@ -0,0 +1,47 @@
+// import {sum, subtract} from "./math";
+// require - to run with node
+const {sum, subtract, sumAsync, subtractAsync} = require('./math');
+
+const test = async (title, callback) => {
+  try {
+    await callback();
+    console.log(`✓ ${title}`);
+
+  } catch (error) {
+    console.error(`✘ ${title}`);
+    console.error(error);
+  }
+};
+
+const expect = result => (
+  {
+    toBe(expected) {
+      if (result !== expected) {
+        throw new Error(`${result} is not equal to ${expected}`);
+      }
+    },
+    toBeGreaterThen() {
+    },
+  }
+);
+
+test('sum: adds numbers', () => {
+  const result = sum(3, 7);
+  const expected = 10;
+
+  expect(result).toBe(expected);
+});
+
+test('subtract: subtract numbers', () => {
+  const result = subtract(3, 7);
+  const expected = -4;
+
+  expect(result).toBe(expected);
+});
+
+test('sumAsync: adds numbers asynchronously', async () => {
+  const result = await sumAsync(3, 7);
+  const expected = 6;
+
+  expect(result).toBe(expected);
+});
---
+++ 1-fundumentals-of-testing/math.js
@@ -0,0 +1,9 @@
+const sum = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+
+const sumAsync = (...args) => Promise.resolve(sum(...args));
+const subtractAsync = (...args) => Promise.resolve(subtract(...args));
+
+// export {sum, subtract, sumAsync, subtractAsync};
+// module.exports - to run with node
+module.exports = {sum, subtract, sumAsync, subtractAsync};
