aee5beb655225d9b3fef0e9f947262edb3d8a8c9
sivanova <s.ivanova@adsterra.com>
2022-07-12 00:24:26 +0300

    2-static-analisys add prettier and typescript

--- 2-static-analisys/src/example.js
+++ 2-static-analisys/src/example.js
@@ -8,5 +8,7 @@ if (!('serviceWorker' in navigator)) {
 }
 
 const greeting = 'hello';
+
 console.log(`${greeting} world!`);
+
 [1, 2, 3].forEach((x) => console.log(x));
---
+++ 2-static-analisys/src/tyscript-example.ts
@@ -0,0 +1,23 @@
+/** @format */
+
+type User = {
+  name: {
+    first: string;
+    middle: string;
+    last: string;
+  };
+};
+
+const add = (a: number, b: number) => a + b;
+
+function getFullName(user: User): string {
+  const {
+    name: { first, middle, last },
+  } = user;
+
+  return [first, middle, last].filter(Boolean).join('');
+}
+
+add(1, 6);
+
+getFullName({ name: { first: 'Joe', middle: 'Bug', last: 'Matthews' } });
