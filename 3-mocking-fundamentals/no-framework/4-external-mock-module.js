3a8bfaf872c16950e5390f723a077a624a813e87
ivveta <s.ivanova@adsterra.com>
2022-09-06 21:22:51 +0300

    add external mocking module

---
+++ 3-mocking-fundamentals/no-framework/4-external-mock-module.js
@@ -0,0 +1,18 @@
+require('../__no-framework-mocks__/utils')
+const utilsPath = require.resolve('../utils')
+const mockUtilsPath = require.resolve('../__no-framework-mocks__/utils')
+require.cache[utilsPath] = require.cache[mockUtilsPath]
+
+const assert = require('assert')
+const thumbWar = require('../thumb-war')
+const utils = require('../utils')
+
+const winner = thumbWar('bmw', 'audi')
+assert.strictEqual(winner, 'bmw')
+assert.deepStrictEqual(utils.getWinner.mock.calls, [
+  [ 'bmw', 'audi' ],
+  [ 'bmw', 'audi' ]
+])
+
+//cleanup
+delete require.cache[utilsPath]
