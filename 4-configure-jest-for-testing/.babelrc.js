287e5177b42c0343949178142c0dc26a0dfc0158
ivveta <s.ivanova@adsterra.com>
2022-09-16 21:57:45 +0300

    add doc

--- 4-configure-jest-for-testing/.babelrc.js
+++ 4-configure-jest-for-testing/.babelrc.js
@@ -1,6 +1,7 @@
 const isProd = String(process.env.NODE_ENV) === 'production'
 const isTest = String(process.env.NODE_ENV) === 'test'
 
+// jest use babel configuration by default
 module.exports = {
   presets: [
     ['@babel/preset-env', {modules: isTest ? 'commonjs' : false}],
