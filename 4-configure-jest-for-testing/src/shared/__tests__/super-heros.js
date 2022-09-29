806f3ed8706f277a3c2f0270fe0f088dd0c81fe0
s.ivanova <s.ivanova@adsterra.com>
2022-09-29 21:50:24 +0300

    add inline snapshot

--- 4-configure-jest-for-testing/src/shared/__tests__/super-heros.js
+++ 4-configure-jest-for-testing/src/shared/__tests__/super-heros.js
@@ -3,5 +3,29 @@ import {getFlyingSuperHeros} from '../super-heros'
 test('returns super heros that can fly', () => {
   const flyingHeros = getFlyingSuperHeros()
 
-  expect(flyingHeros).toMatchSnapshot()
+  expect(flyingHeros).toMatchInlineSnapshot(`
+[
+  {
+    "name": "Dynagay",
+    "powers": [
+      "disintegration ray",
+      "fly",
+    ],
+  },
+  {
+    "name": "Apogee",
+    "powers": [
+      "gravity control",
+      "fly",
+    ],
+  },
+  {
+    "name": "Jack-Jack",
+    "powers": [
+      "shapeshifting",
+      "fly",
+    ],
+  },
+]
+`)
 })
