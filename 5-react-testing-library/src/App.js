cd7deda236991efbeff235367c3215bb0c099f65
s.ivanova <s.ivanova@adsterra.com>
2022-10-05 23:15:50 +0300

    add accessiblity test

---
+++ 5-react-testing-library/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import {FavoriteNumber} from "./favorite-number";
+
+function App() {
+  return (
+    <div>
+      <FavoriteNumber />
+    </div>
+  );
+}
+
+export default App;
