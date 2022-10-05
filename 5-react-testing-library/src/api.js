6fde333daff17c5b1d9d4929d54bcda280a47cef
s.ivanova <s.ivanova@adsterra.com>
2022-10-06 00:11:17 +0300

    mock api

---
+++ 5-react-testing-library/src/api.js
@@ -0,0 +1,3 @@
+export const loadGreeting = async()=>{
+  return 'greeting'
+}
---
+++ 5-react-testing-library/src/greeting-loader-01-mocking.js
@@ -0,0 +1,19 @@
+import {loadGreeting} from './api'
+import {useState} from "react";
+
+export const GreetingLoader = () => {
+  const [greeting, setGreeting] = useState('')
+  async function loadGreetingForInput(e) {
+    e.preventDefault()
+    const {data} = await loadGreeting(e.target.elements.name.value)
+    setGreeting(data.greeting)
+  }
+  return (
+    <form onSubmit={loadGreetingForInput}>
+      <label htmlFor="name">Name</label>
+      <input id="name" />
+      <button type="submit">Load Greeting</button>
+      <div aria-label="greeting">{greeting}</div>
+    </form>
+  )
+}
