8accbf763e89e8247dad5037c818aa16d0815e79
ivveta <s.ivanova@adsterra.com>
2023-08-02 17:22:45 +0300

    test store

--- 5-react-testing-library/src/__tests__/8-use-store.js
+++ 5-react-testing-library/src/__tests__/8-use-store.js
@@ -3,6 +3,8 @@ import userEvent from '@testing-library/user-event';
 import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from '../8-use-store/store';
+import { configureStore } from '@reduxjs/toolkit';
+import { counterSlice } from '../8-use-store/slice';
 
 test('can render with redux with defaults', () => {
   render(
@@ -14,4 +16,23 @@ test('can render with redux with defaults', () => {
   userEvent.click(screen.getByText('+'));
 
   expect(screen.getByLabelText(/count/i)).toHaveTextContent('1');
+});
+
+test('can render with redux with custom initial state', () => {
+  const customStore = configureStore({
+    reducer: {
+      counter: counterSlice,
+    },
+    preloadedState: { counter: { count: 3 } },
+  });
+
+  render(
+    <Provider store={customStore}>
+      <Counter />
+    </Provider>,
+  );
+
+  userEvent.click(screen.getByText('-'));
+
+  expect(screen.getByLabelText(/count/i)).toHaveTextContent('2');
 });
